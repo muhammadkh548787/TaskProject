9e4d2d26b9a64329909c3b5ff6fc358d626cb345
muhammadkh548787 <146752397+muhammadkh548787@users.noreply.github.com>
2023-11-09 14:42:57 +0500

    Update prowild.py

--- prowild.py
+++ prowild.py
@@ -6,6 +6,16 @@ import os
 from pathlib import Path
 import shutil
 import hashlib
+import datetime
+from datetime import datetime
+date_string = datetime.today()
+day = "%d"
+month = "%m"
+year = "20%y"
+day_now = int(datetime.strftime(date_string,day))
+month_now = int(datetime.strftime(date_string,month))
+year_now = int(datetime.strftime(date_string, year))
+
 
 class Add:
     def __init__(self, cwd):
@@ -13,7 +23,7 @@ class Add:
     def reg(self):
         user = input("User: ")
         password = input("Password: ")
-        file_name = 'empty.txt'
+        file_name = 'userp.txt'
         with open(file_name, 'a') as f:
             f.write((user)+ '\n')
         hash_object = hashlib.sha256()
@@ -30,11 +40,11 @@ class Add:
         task_status = input("Status of your task: ")
         task = f"number: {task_number} | name: {task_name} | description: {task_description} | priority: {task_priority} | dedline: {task_dedline} | status: {task_status}"
         
-        with open("empty.txt", 'a') as f:
+        with open("tasks.txt", 'a') as f:
             f.write(task + "\n")
 
     def show_tasks(self):
-        qw = Path('empty.txt')
+        qw = Path('tasks.txt')
         file_content = qw.read_text()
         print(file_content)
     def delete_task(self):
@@ -45,12 +55,20 @@ class Add:
             ad.remove_task(task)
             print("The task has been deleted")
     def todays_task(self):
-        today = date.today()
-        with open("empty.txt", 'r') as f:
-            for i in f:
-                line_split = i.split()
-                if line_split == today:
-                    print(line_split)
+        with open("tasks.txt","r") as file:
+            for line in file.read().splitlines():
+                line_split = line.split("|")
+                line_split_1 = str(line_split[4])
+                line_split_1_1 = line_split_1.split(":")
+                line_split_1_1_1 = line_split_1_1[1]
+                line_splited_to_str_nums = line_split_1_1_1.split("-")
+                line_splited_to_str_nums_day = int(line_splited_to_str_nums[0])
+                line_splited_to_str_nums_month = int(line_splited_to_str_nums[1])
+                line_splited_to_str_nums_year = int(line_splited_to_str_nums[2])
+                if line_splited_to_str_nums_day == day_now and line_splited_to_str_nums_month == month_now and line_splited_to_str_nums_year == year_now:
+                    print(line)
+
+
 
 
 
